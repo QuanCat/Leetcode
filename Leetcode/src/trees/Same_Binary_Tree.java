34c78f212f527358dc84235f477d28506f94e451
QuanCat <buptquanquan@gmail.com>
2014-09-11 15:00:02 -0400

    add check same binary tree

---
+++ Leetcode/src/trees/Same_Binary_Tree.java
@@ -0,0 +1,26 @@
+package trees;
+
+public class Same_Binary_Tree {
+	public class TreeNode{
+		int val;
+		TreeNode left;
+		TreeNode right;
+		TreeNode(int x) {
+			val = x;
+		}
+	}
+	public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+        	return true;
+        } else if (p != null && q != null) {
+        	if (p.val == q.val) {
+        		return isSameTree(p.left, q.left) &&
+        				isSameTree(p.right, q.right);
+        	}
+        } else {
+        	return false;
+        }
+        return false;
+        
+    }
+}
